c41c73fb7c7f52cb4721a181e744c952dd9c3a4f
Zack Leonardo <zackleonardo777@gmail.com>
2024-06-10 13:48:29 -0700

    Added Exception Handling and modified the calculator

---
+++ ConsoleApp.ExceptionHandlingDemo/Program.cs
@@ -0,0 +1,28 @@
+﻿namespace ConsoleApp.ExceptionHandlingDemo
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //Write a program that takes a user's name as input and prints it to the screen.
+            //Display an error message if an invalid input is recieved
+            try
+            {
+                Console.Write("Enter your age: ");
+                int age = Convert.ToInt32(Console.ReadLine());
+
+                Console.WriteLine($"You are {age} years old");
+            }
+            catch (Exception)
+            {
+                Console.WriteLine("Your Age value was incorrect, please try again");
+                //throw;
+            }
+            finally
+            {
+                Console.WriteLine("***************-Thank You for using this program-*********************");
+            }
+            
+        }
+    }
+}
--- ConsoleApp.SimpleCalculator/Program.cs
+++ ConsoleApp.SimpleCalculator/Program.cs
@@ -4,89 +4,97 @@
     {
         static void Main(string[] args)
         {
-            //Welcome message
-            Console.WriteLine("**********- Welcome to the simple calculator -**********");
+            
+            //Variables
+            int choice = 0;
+            int num1, num2 = 0;
 
-            //Show calculator options
-            Console.WriteLine("****- Operations Menu -*****");
-            Console.WriteLine();
-
-            Console.WriteLine("Please select an operation(-1 to exit)");
-            Console.WriteLine("1 - Addition");
-            Console.WriteLine("2 - Subtraction");
-            Console.WriteLine("3 - Multiplication");
-            Console.WriteLine("4 - Division");
-            Console.WriteLine("5 - FibonacciSequence");
-            Console.WriteLine();
-
-            Console.Write("Choice: ");
-            int choice = Convert.ToInt32(Console.ReadLine());
             while (choice != -1)
             {
-                //Prompt User Input
-                Console.Write("Please enter the first number: ");
-                int num1 = Convert.ToInt32(Console.ReadLine());
-
-                //Prompt for second number
-                Console.Write("Please enter the second number: ");
-                int num2 = Convert.ToInt32(Console.ReadLine());
-
-                Console.WriteLine();
-
-                int answer = 0;
-                //Decide which operation is needed based on option selected
-                switch (choice)
+                try
                 {
-                    case 1:
-                        answer = num1 + num2;
-                        Console.WriteLine($"{num1} + {num2}");
+                    //Welcome message
+                    Console.Clear();
+                    Console.WriteLine("**********- Welcome to the simple calculator -**********");
+                    Console.WriteLine();
+                    Console.WriteLine("Please select an operation(-1 to exit)");
+                    Console.WriteLine("1 - Addition");
+                    Console.WriteLine("2 - Subtraction");
+                    Console.WriteLine("3 - Multiplication");
+                    Console.WriteLine("4 - Division");
+                    Console.WriteLine("5 - Fibonacci Sequence");
+                    Console.WriteLine();
+                    Console.Write("Choice: ");
+                    choice = Convert.ToInt32(Console.ReadLine());
+
+                    if (choice == -1)
+                    {
                         break;
-                    case 2:
-                        answer = num1 - num2;
-                        Console.WriteLine($"{num1} - {num2}");
-                        break;
-                    case 3:
-                        answer = num1 * num2;
-                        Console.WriteLine($"{num1} * {num2}");
-                        break;
-                    case 4:
-                        answer = num1 / num2;
-                        Console.WriteLine($"{num1} / {num2}");
-                        break;
-                    case 5:
-                        for (int i = num1; i <= num2; i++)
-                        {
-                            answer += i;
-                        }
-                        Console.WriteLine($"Sum of numbers from {num1} to {num2}");
-                        break;
-                    default:
-                        Console.WriteLine("Not a Valid Choice!");
-                        break;
+                    }
+
+                    //Prompt User Input
+                    Console.Write("Please enter the first number: ");
+                    num1 = Convert.ToInt32(Console.ReadLine());
+
+                    //Prompt for second number
+                    Console.Write("Please enter the second number: ");
+                    num2 = Convert.ToInt32(Console.ReadLine());
+
+                    Console.WriteLine();
+
+                    int answer = 0;
+                    //Decide which operation is needed based on option selected
+                    switch (choice)
+                    {
+                        case 1:
+                            answer = num1 + num2;
+                            Console.WriteLine($"{num1} + {num2}");
+                            break;
+                        case 2:
+                            answer = num1 - num2;
+                            Console.WriteLine($"{num1} - {num2}");
+                            break;
+                        case 3:
+                            answer = num1 * num2;
+                            Console.WriteLine($"{num1} * {num2}");
+                            break;
+                        case 4:
+                            answer = num1 / num2;
+                            Console.WriteLine($"{num1} / {num2}");
+                            break;
+                        case 5:
+                            for (int i = num1; i <= num2; i++)
+                            {
+                                answer += i;
+                            }
+                            Console.WriteLine($"Sum of numbers from {num1} to {num2}");
+                            break;
+                        default:
+                            throw new Exception("Invalid Menu Item Selected.");
+                    }
+
+                    //Print Answer output
+                    Console.WriteLine($"Your Answer is: {answer}");
                 }
-                //Print output
+                catch (DivideByZeroException)
+                {
+                    Console.WriteLine("Cannot Divide by zero");
+                    
+                }
+                catch (Exception ex)
+                {
+                    Console.WriteLine(ex.Message);
 
-                Console.WriteLine($"Your Answer is: {answer}");
-                Console.WriteLine("Press enter to continue");
-                Console.ReadLine();
-                Console.Clear();
-                //Show calculator options
-                Console.WriteLine("****- Operations Menu -*****");
-                Console.WriteLine();
-
-                Console.WriteLine("Please select an operation(-1 to exit)");
-                Console.WriteLine("1 - Addition");
-                Console.WriteLine("2 - Subtraction");
-                Console.WriteLine("3 - Multiplication");
-                Console.WriteLine("4 - Division");
-                Console.WriteLine("5 - FibonacciSequence");
-                Console.WriteLine();
-                Console.Write("Choice: ");
-                choice = Convert.ToInt32(Console.ReadLine());
+                }
+                finally
+                {
+                    Console.WriteLine();
+                    Console.WriteLine("Press any key to continue.");
+                    Console.ReadLine();
+                }
             }
-
-            Console.WriteLine("Thank you for using the simple calculator program");
-            Environment.Exit(0);
+            Console.WriteLine();
+            Console.WriteLine("******- Thank you for using the simple calculator program -********");
         }
     }
 }
