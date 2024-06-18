adfdff0e17829e014d7d6219ba1e69aaf261d94b
Zack Leonardo <zackleonardo777@gmail.com>
2024-06-18 13:53:35 -0700

    Added Arrays and Lists Demo

---
+++ ConsoleApp.Arrays/Program.cs
@@ -0,0 +1,63 @@
+﻿namespace ConsoleApp.Arrays
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("*********************** - ARRAYS - ************************");
+
+            //Tell me how many student and grade are to be entered
+            Console.WriteLine("The number of grades to be entered: ");
+            int numberOfGrades = Convert.ToInt32(Console.ReadLine());
+
+            //Declare Fixed Size Array
+            int[] grades = new int[numberOfGrades];
+            string[] students = new string[numberOfGrades];
+
+            //5 array indexes/adresses - 0,1,2,3,4
+            //if n is the size of the array, then your array has addresses between 0 - n-1
+
+            //Add Values to Fixed Size Array
+            //grades[0] = 45;
+            //grades[1] = 25;
+            //grades[2] = 38;
+            //grades[3] = 45;
+            //grades[4] = 54;
+
+            for (int i = 0; i < numberOfGrades; i++)
+            {
+                Console.Write("Please enter student's name: ");
+                students[i] = Console.ReadLine();
+                
+                Console.Write("Please enter student's grade: ");
+                grades[i] = Convert.ToInt32(Console.ReadLine());
+            }
+
+            //Print Values From Fixed Size Array
+            Console.WriteLine("The grades you have entered are: ");
+            for (int i = 0; i < grades.Length; i++)
+            {
+                Console.WriteLine($"{students[i]}: recieved a: {grades[i]}%");    
+            }
+
+            //Declare Variable Sized Array
+            //string[] studentNames = { "bob", "Frank", "Chris", "Etc..."};
+
+
+            ////Add Values to Variable Size Array
+            //for (int i = 0; i < studentNames.Length; i++)
+            //{
+            //    Console.Write("Please enter student name: ");
+            //    studentNames[i] = Console.ReadLine();
+            //}
+
+
+            ////Print Values From Variable Size Array
+            //Console.WriteLine("The names you have entered are: ");
+            //for (int i = 0; i < studentNames.Length; i++)
+            //{
+            //    Console.WriteLine(studentNames[i]);
+            //}
+        }
+    }
+}
---
+++ ConsoleApp.Lists/Program.cs
@@ -0,0 +1,55 @@
+﻿namespace ConsoleApp.Lists
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //Declare a list
+            List<int> grades = new List<int>();
+            List<string> students = new List<string>();
+            int grade = 0;
+            int gradeCount = 0;
+            string name;
+            int @continue;
+            //var grades2 = new List<int>();  
+            //List<int> grades3 = new();
+
+            //Add values to a list
+            //grades.Add(35);
+
+            do
+            {
+                gradeCount += 1;
+                Console.Write("Please enter a student's name: ");
+                name = Console.ReadLine();
+                students.Add(name);
+
+                Console.Write("Please enter a student's grade: ");
+                grade = Convert.ToInt32(Console.ReadLine());
+                grades.Add(grade);
+                //if (grade != -1)
+                //{
+                //    grades.Add(grade); 
+                //}
+
+                Console.Write("Do you wish to continue?(1 = yes | 2 = no): ");
+                @continue = Convert.ToInt32(Console.ReadLine());
+
+            } while (@continue == 1);
+
+            //Print values in a list - For
+            Console.WriteLine("Printing names via FOR loop");
+            for (int i = 0; i < gradeCount; i++)
+            {
+                Console.WriteLine($"Student: {students[i]} Recieved:{grades[i]}%");
+            }
+
+            //Print values in a list - Foreach
+            //Console.WriteLine("Printing names via FOREACH loop");
+            //foreach (int g in grades) 
+            //{
+            //    Console.WriteLine(g);
+            //}
+        }
+    }
+}
